e869db02c48a788b8657655f8648a8ddbf481090
Carlos Eli Lopez Tellez <carloselilopezt@gmail.com>
2019-09-17 15:36:55 -0500

    Removed contact form temporarily

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,10 @@ Route::get('about', function() {
     return view('about');
 })->name('about');
 
-Route::get('contact', function() {
-    return view('contact');
-})->name('contact');
+//TODO: Contact form
+// Route::get('contact', function() {
+//     return view('contact');
+// })->name('contact');
 
 Auth::routes();
 
